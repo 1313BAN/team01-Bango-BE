bba0b8614f5030bab6de7dec0dec19ebda6f3269
dpfls0922 <dpfls0922@naver.com>
2025-05-12 00:58:49 +0900

    [CHORE] DTO에 @Builder 적용 #1

--- src/main/java/com/ssafy/bango/domain/rentalhouse/dto/RentalHouse.java
+++ src/main/java/com/ssafy/bango/domain/rentalhouse/dto/RentalHouse.java
@@ -2,6 +2,7 @@ package com.ssafy.bango.domain.rentalhouse.dto;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class RentalHouse {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
