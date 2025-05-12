8e36ebfdb05f6392f54dd69a4c907bf694f546a9
dpfls0922 <dpfls0922@naver.com>
2025-05-12 16:01:52 +0900

    [FEAT] MethodArgumentNotValidException 예외 처리 추가 #1

--- src/main/java/com/ssafy/bango/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/bango/global/exception/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.ssafy.bango.global.exception;
 
+import static com.ssafy.bango.global.exception.enums.ErrorType.INVALID_TYPE_ERROR;
+
 import com.ssafy.bango.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +17,21 @@ public class GlobalExceptionHandler {
     /**
      * CUSTOM_ERROR
      */
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<String>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .findFirst()
+            .orElse("잘못된 요청입니다.");
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(INVALID_TYPE_ERROR, errorMessage));
+    }
+
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ApiResponse<?>> handleCustomException(CustomException e) {
 
