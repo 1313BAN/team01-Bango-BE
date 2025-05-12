7b2bd451a677c74781d9a054ce2f4c9987724d5e
정지윤 <alex68566856@gmail.com>
2025-05-12 16:35:26 +0900

    [FEAT] GlobalExceptionHandler 기본적인 예외 추가

--- src/main/java/com/ssafy/bango/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/bango/global/exception/GlobalExceptionHandler.java
@@ -1,14 +1,113 @@
 package com.ssafy.bango.global.exception;
 
 import com.ssafy.bango.global.common.ApiResponse;
+import com.ssafy.bango.global.exception.enums.ErrorType;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.UnexpectedTypeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ssafy.bango.global.exception.enums.ErrorType.*;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    /**
+     * 400 VALIDATION_ERROR
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ApiResponse<?> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+
+        Errors errors = e.getBindingResult();
+        Map<String, String> validateDetails = new HashMap<>();
+
+        for (FieldError error : errors.getFieldErrors()) {
+            String validKeyName = String.format("valid_%s", error.getField());
+            validateDetails.put(validKeyName, error.getDefaultMessage());
+        }
+        return ApiResponse.error(REQUEST_VALIDATION_ERROR, validateDetails);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UnexpectedTypeException.class)
+    protected ApiResponse<?> handleUnexprectedTypeException(final UnexpectedTypeException e) {
+        return ApiResponse.error(INVALID_TYPE_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ApiResponse<?> handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        return ApiResponse.error(INVALID_TYPE_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    protected ApiResponse<?> handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
+        return ApiResponse.error(INVALID_MISSING_HEADER_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ApiResponse<?> handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
+        return ApiResponse.error(INVALID_HTTP_REQUEST_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ApiResponse<?> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        return ApiResponse.error(INVALID_HTTP_METHOD_ERROR);
+    }
+
+
+    /**
+     * 500 INTERNEL_SERVER_ERROR
+     */
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    protected ApiResponse<Exception> handleException(final Exception e, final HttpServletRequest request) {
+        return ApiResponse.error(ErrorType.INTERNAL_SERVER_ERROR, e);
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ApiResponse<Exception> handleIllegalArgumentException(final IllegalArgumentException e, final HttpServletRequest request) {
+        return ApiResponse.error(ErrorType.INTERNAL_SERVER_ERROR, e);
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(IOException.class)
+    public ApiResponse<Exception> handleIOException(final IOException e, final HttpServletRequest request) {
+
+        return ApiResponse.error(ErrorType.INTERNAL_SERVER_ERROR, e);
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public ApiResponse<Exception> handleRuntimeException(final RuntimeException e, final HttpServletRequest request) {
+        if (e.getMessage() != null) {
+            return ApiResponse.error(ErrorType.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        } else {
+            return ApiResponse.error(ErrorType.INTERNAL_SERVER_ERROR, e);
+        }
+    }
+
     /**
      * CUSTOM_ERROR
      */
--- src/main/java/com/ssafy/bango/global/exception/enums/ErrorType.java
+++ src/main/java/com/ssafy/bango/global/exception/enums/ErrorType.java
@@ -49,7 +49,12 @@ public enum ErrorType {
      * 409 CONFLICT
      */
     NICKNAME_DUP_ERROR(HttpStatus.CONFLICT, "중복된 회원 닉네임입니다."),
-    MEMBER_ALREADY_EXIST_ERROR(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다.");
+    MEMBER_ALREADY_EXIST_ERROR(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
+
+    /**
+     * 500 INTERNAL_SERVER_ERROR
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
 
     private final HttpStatus httpStatus;
