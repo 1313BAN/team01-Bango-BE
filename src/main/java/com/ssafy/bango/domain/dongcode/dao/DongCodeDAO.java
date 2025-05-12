7c75ca034e9d40eb22fe5618a068709fcbd8b4c2
dpfls0922 <dpfls0922@naver.com>
2025-05-12 16:32:46 +0900

    [DEL] 불필요한 메서드 삭제 #1

--- src/main/java/com/ssafy/bango/domain/dongcode/dao/DongCodeDAO.java
+++ src/main/java/com/ssafy/bango/domain/dongcode/dao/DongCodeDAO.java
@@ -15,7 +15,4 @@ public interface DongCodeDAO extends JpaRepository<DongCode, String> {
     @Query("SELECT DISTINCT d.gugunName FROM DongCode d WHERE d.sidoName = :sidoName AND d.gugunName IS NOT NULL ORDER BY d.gugunName")
     List<String> getDistinctGugunBySidoName(String sidoName);
     List<DongCode> findBySidoNameAndGugunNameAndDongNameIsNotNullOrderByDongName(String sidoName, String gugunName);
-
-    @Query("SELECT DISTINCT d.dongCode FROM DongCode d WHERE d.dongName = :dongName")
-    String findDongCodeByDongName(@Param("dongName") String dongName);
 }
