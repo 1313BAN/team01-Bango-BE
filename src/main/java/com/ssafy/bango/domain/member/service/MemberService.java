0cd2c35295eeacc117026409701e4a8e3d7205da
정지윤 <alex68566856@gmail.com>
2025-05-13 14:36:57 +0900

    [REFACTOR] 회원가입과 회원 정보 조회를 코드 통합 (유저가 없을 경우 무조건 가입하기 때문) #14

--- src/main/java/com/ssafy/bango/domain/member/service/MemberService.java
+++ src/main/java/com/ssafy/bango/domain/member/service/MemberService.java
@@ -31,16 +31,12 @@ public class MemberService {
         String accessToken = loginRequest.socialAccessToken();
         SocialPlatform socialPlatform = loginRequest.socialPlatform();
 
-        // 유저 정보가 없을 경우 회원가입
         String socialId = oAuthService.getSocialId(socialPlatform, accessToken);
-        if (!memberRepository.existsBySocialId(socialId)) {
-            signUp(socialPlatform, socialId, accessToken);
-        }
 
-        // DB에 유저 정보를 조회하고, 없을 경우 예외
+        // DB에 유저 정보를 조회하고, 없을 경우 회원 가입
         Member loginMember = memberRepository
                 .getMemberBySocialId(socialId)
-                .orElseThrow(() -> new CustomException(ErrorType.NOT_FOUND_MEMBER_ERROR));
+                .orElseGet(() -> signUp(socialPlatform, socialId, accessToken));
 
         // 토큰 발행
         TokenDTO tokenDTO = jwtProvider.issueToken(
@@ -53,7 +49,7 @@ public class MemberService {
     /**
      * 멤버를 생성하고, kakao/google로부터 받아온 정보 설정.
      */
-    private void signUp(SocialPlatform socialPlatform, String socialId, String accessToken) {
+    private Member signUp(SocialPlatform socialPlatform, String socialId, String accessToken) {
         Member member = Member.builder()
                 .socialPlatform(socialPlatform)
                 .socialId(socialId)
@@ -61,6 +57,8 @@ public class MemberService {
 
         memberRepository.save(member);
         oAuthService.setSocialInfo(socialPlatform, accessToken, member);
+
+        return member;
     }
 
     @Transactional
