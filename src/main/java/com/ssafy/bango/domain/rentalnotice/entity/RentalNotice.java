d402579054a94657530d819aba0c321df69e5785
J2yun <alex68566856@gmail.com>
2025-05-14 18:35:19 +0900

    [FEAT] builder 추가, batch 처리에 필요한 칼럼 추가 #20

--- src/main/java/com/ssafy/bango/domain/rentalnotice/entity/RentalNotice.java
+++ src/main/java/com/ssafy/bango/domain/rentalnotice/entity/RentalNotice.java
@@ -4,21 +4,29 @@ import com.ssafy.bango.domain.noticelikes.entity.NoticeLike;
 import com.ssafy.bango.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "rental_notice")
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "rental_notice")
 public class RentalNotice extends BaseTimeEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer noticeId;
+
+  // 주택 공고 api 상 id (pblancId)
+  private String pblancId;
+
+  private String noticeName;
 
   @Column(length = 10)
   private String institutionType;
@@ -32,9 +40,9 @@ public class RentalNotice extends BaseTimeEntity {
   @Column(length = 30)
   private String supplyType;
 
-  private LocalDateTime createdDate;
+  private LocalDate createdDate;
 
-  private LocalDateTime announceDate;
+  private LocalDate announceDate;
 
   @Column(length = 30)
   private String supplyHoCount;
@@ -54,9 +62,15 @@ public class RentalNotice extends BaseTimeEntity {
   @Column(length = 30)
   private String gugunName;
 
-  private LocalDateTime beginDate;
+  @Column(length = 20)
+  private String pnu;
 
-  private LocalDateTime endDate;
+  private LocalDate beginDate;
+
+  private LocalDate endDate;
+
+  // 주택 공고 동기화 시점
+  private LocalDateTime lastSyncedAt;
 
   @OneToMany(mappedBy = "rentalNotice", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<NoticeLike> likes;
