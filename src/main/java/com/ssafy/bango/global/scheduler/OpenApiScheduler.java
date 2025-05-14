240b17802b5d3700ad9d6d34a85990332ddec9e4
dpfls0922 <dpfls0922@naver.com>
2025-05-14 23:55:18 +0900

    [FEAT] Batch Job에 대해 스케줄러 등록 #6

---
+++ src/main/java/com/ssafy/bango/global/scheduler/OpenApiScheduler.java
@@ -0,0 +1,32 @@
+package com.ssafy.bango.global.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OpenApiScheduler {
+    private final Job batchJob;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled(cron = "0 0 4 * * ?")
+    public void runBatch() {
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLong("run.id", System.currentTimeMillis())
+                    .toJobParameters();
+
+            jobLauncher.run(batchJob, params);
+            log.info("[OpenApiScheduler] 공공 API 배치 실행 완료");
+        } catch (Exception e) {
+            log.error("[OpenApiScheduler] 공공 API 배치 실행 실패", e);
+        }
+    }
+}
