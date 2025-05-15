9faddcecca07d24b95900e98f91a98f7667e5209
J2yun <alex68566856@gmail.com>
2025-05-15 10:46:08 +0900

    [FEAT] 생성자 private으로(인스턴스화 방지), 예외처리 추가 #20

--- src/main/java/com/ssafy/bango/global/util/DateUtil.java
+++ src/main/java/com/ssafy/bango/global/util/DateUtil.java
@@ -1,15 +1,26 @@
 package com.ssafy.bango.global.util;
 
 
+import com.ssafy.bango.global.exception.CustomException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DateUtil {
 
     public static final DateTimeFormatter YYYYMMDD_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     public static LocalDate parseYyyyMMdd(String dateStr) {
-        return LocalDate.parse(dateStr, YYYYMMDD_FORMATTER);
+        try {
+            return LocalDate.parse(dateStr, YYYYMMDD_FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("날짜 형식이 올바르지 않습니다: " + dateStr, e);
+        }
+
     }
 
 //    public static String formatYyyyMMdd(LocalDate date) {
