08442b4adb15a00ee00c8a96d3f268b0e399dc50
J2yun <alex68566856@gmail.com>
2025-05-16 01:22:05 +0900

    [FEAT] spring cache 의존성 추가 및, redis를 캐시 스토리지로 사용하기 위한 세팅 #6

--- src/main/java/com/ssafy/bango/global/config/RedisConfig.java
+++ src/main/java/com/ssafy/bango/global/config/RedisConfig.java
@@ -1,13 +1,21 @@
 package com.ssafy.bango.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Configuration
-
+@EnableCaching
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
@@ -20,4 +28,18 @@ public class RedisConfig {
         return new LettuceConnectionFactory(host, port);
     }
 
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        // value 직렬화: JSON, null 값은 캐싱하지 않음
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(32)) // 매달 batch 실행하므로
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+    }
 }
