44f764ad144b5238413f3674e14fe80408b8aa3e
dpfls0922 <dpfls0922@naver.com>
2025-05-23 20:41:18 +0900

    [ADD] swagger 관련 url 추가 #27

--- src/main/java/com/ssafy/bango/global/auth/security/AuthWhiteList.java
+++ src/main/java/com/ssafy/bango/global/auth/security/AuthWhiteList.java
@@ -6,21 +6,25 @@ import java.util.stream.Stream;
 
 public class AuthWhiteList {
     public static final List<String> AUTH_WHITELIST_DEFAULT = Arrays.asList(
-            "/api/v1/member/login", "/api/v1/member/token",
-            "/api/v1/rental",
-            "/api/v1/notice",
-            "/batch-rentalhouse-job", "/batch-facility-job",
-            "/error", "/health"
+        "/api/v1/member/login", "/api/v1/member/token",
+        "/api/v1/rental",
+        "/api/v1/notice",
+        "/batch-rentalhouse-job", "/batch-facility-job",
+        "/error", "/health",
+        "/swagger-ui.html"
     );
 
     public static final List<String> AUTH_WHITELIST_WILDCARD = Arrays.asList(
         "/api/v1/rental/**",
         "/api/v1/rental/api/**",
-        "/api/v1/ai/**"
+        "/api/v1/ai/**",
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "/swagger-resources/**"
     );
 
     public static final String[] AUTH_WHITELIST = Stream.concat(
-            AUTH_WHITELIST_DEFAULT.stream(),
-            AUTH_WHITELIST_WILDCARD.stream()
+        AUTH_WHITELIST_DEFAULT.stream(),
+        AUTH_WHITELIST_WILDCARD.stream()
     ).toArray(String[]::new);
 }
