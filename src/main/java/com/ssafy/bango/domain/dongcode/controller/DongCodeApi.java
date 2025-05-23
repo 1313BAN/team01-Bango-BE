8ee046bcefa85484c33602dd40c80dc1b08cc6ee
dpfls0922 <dpfls0922@naver.com>
2025-05-23 21:12:15 +0900

    [ADD] 스웨거 설명 어노테이션 추가 #27

--- src/main/java/com/ssafy/bango/domain/dongcode/controller/DongCodeApi.java
+++ src/main/java/com/ssafy/bango/domain/dongcode/controller/DongCodeApi.java
@@ -1,13 +1,27 @@
 package com.ssafy.bango.domain.dongcode.controller;
 
 import com.ssafy.bango.domain.dongcode.entity.DongCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@Tag(name = "동 코드 컨트롤러", description = "동 코드 관련 API입니다.")
 public interface DongCodeApi {
+    @Operation(summary = "시도 조회", description = "모든 시도를 조회하는 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "시도 조회 완료")
+    })
     ResponseEntity<List<String>> getDistinctSido();
+
+    @Operation(summary = "구군 조회", description = "모든 구군을 조회하는 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "구군 조회 완료")
+    })
     ResponseEntity<List<String>> getDistinctGugun(@RequestParam String sidoName);
+
+    @Operation(summary = "동 조회", description = "모든 동을 조회하는 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "동 조회 완료")
+    })
     ResponseEntity<List<DongCode>> getDongCode(@RequestParam String sidoName, @RequestParam String gugunName);
 }
--- src/main/java/com/ssafy/bango/domain/noticelike/controller/NoticeLikeApi.java
+++ src/main/java/com/ssafy/bango/domain/noticelike/controller/NoticeLikeApi.java
@@ -3,14 +3,26 @@ package com.ssafy.bango.domain.noticelike.controller;
 import com.ssafy.bango.domain.noticelike.dto.response.LikedNoticeResponse;
 import com.ssafy.bango.domain.noticelike.entity.NoticeLike;
 import com.ssafy.bango.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.security.Principal;
 
+@Tag(name = "공고 찜하기 컨트롤러", description = "공고 찜하기 관련 API입니다.")
 public interface NoticeLikeApi {
+    @Operation(summary = "공고 찜하기", description = "공고 찜하기 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "찜하기 완료")
+    })
     ResponseEntity<ApiResponse<NoticeLike>> addLikedNotice(Integer noticeId, Principal principal);
 
+    @Operation(summary = "공고 찜해제", description = "공고 찜해제 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "찜해제 완료")
+    })
     ResponseEntity<ApiResponse<?>> deleteLikedNotice(Integer noticeId, Principal principal);
 
+    @Operation(summary = "공고 찜한 목록 조회", description = "공고 찜한 목록 조회 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "공고 찜한 목록 조회 완료")
+    })
     ResponseEntity<ApiResponse<LikedNoticeResponse>> getLikedNoticeList(Principal principal);
 }
