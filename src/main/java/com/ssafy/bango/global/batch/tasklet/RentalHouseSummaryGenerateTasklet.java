0d862b96207b92982ccf92ba0b3cbb729579b1a2
dpfls0922 <dpfls0922@naver.com>
2025-05-26 01:15:25 +0900

    [FIX] @Transactional import 변경 #18

--- src/main/java/com/ssafy/bango/global/batch/tasklet/RentalHouseSummaryGenerateTasklet.java
+++ src/main/java/com/ssafy/bango/global/batch/tasklet/RentalHouseSummaryGenerateTasklet.java
@@ -1,7 +1,7 @@
 package com.ssafy.bango.global.batch.tasklet;
 
 import com.ssafy.bango.domain.rentalhouse.service.RentalHouseSummaryService;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
