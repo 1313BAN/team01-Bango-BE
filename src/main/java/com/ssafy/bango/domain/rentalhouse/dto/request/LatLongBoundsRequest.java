c8055aca8174bb2c92beb1ecb948c627e45c44f0
dpfls0922 <dpfls0922@naver.com>
2025-05-25 19:50:28 +0900

    [REFACTOR] 서울 위도/경도 범위 유효성 검사 어노테이션 추가 및 적용 #18

--- src/main/java/com/ssafy/bango/domain/rentalhouse/dto/request/LatLongBoundsRequest.java
+++ src/main/java/com/ssafy/bango/domain/rentalhouse/dto/request/LatLongBoundsRequest.java
@@ -1,5 +1,6 @@
 package com.ssafy.bango.domain.rentalhouse.dto.request;
 
+import com.ssafy.bango.global.validation.ValidLatLongBounds;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
@@ -8,24 +9,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@ValidLatLongBounds
 public class LatLongBoundsRequest {
     @NotNull(message = "최소 위도는 필수입니다.")
-    @DecimalMin(value = "33.0", message = "대한민국 범위의 최소 위도여야 합니다.")
-    @DecimalMax(value = "43.0", message = "대한민국 범위의 최대 위도여야 합니다.")
+    @DecimalMin(value = "37.4", message = "서울 범위의 최소 위도여야 합니다.")
+    @DecimalMax(value = "37.7", message = "서울 범위의 최대 위도여야 합니다.")
     private Double minLatitude;
 
     @NotNull(message = "최대 위도는 필수입니다.")
-    @DecimalMin(value = "33.0", message = "대한민국 범위의 최소 위도여야 합니다.")
-    @DecimalMax(value = "43.0", message = "대한민국 범위의 최대 위도여야 합니다.")
+    @DecimalMin(value = "37.4", message = "서울 범위의 최소 위도여야 합니다.")
+    @DecimalMax(value = "37.7", message = "서울 범위의 최대 위도여야 합니다.")
     private Double maxLatitude;
 
     @NotNull(message = "최소 경도는 필수입니다.")
-    @DecimalMin(value = "124.0", message = "대한민국 범위의 최소 경도여야 합니다.")
-    @DecimalMax(value = "132.0", message = "대한민국 범위의 최대 경도여야 합니다.")
+    @DecimalMin(value = "126.7", message = "서울 범위의 최소 경도여야 합니다.")
+    @DecimalMax(value = "127.2", message = "서울 범위의 최대 경도여야 합니다.")
     private Double minLongitude;
 
     @NotNull(message = "최대 경도는 필수입니다.")
-    @DecimalMin(value = "124.0", message = "대한민국 범위의 최소 경도여야 합니다.")
-    @DecimalMax(value = "132.0", message = "대한민국 범위의 최대 경도여야 합니다.")
+    @DecimalMin(value = "126.7", message = "서울 범위의 최소 경도여야 합니다.")
+    @DecimalMax(value = "127.2", message = "서울 범위의 최대 경도여야 합니다.")
     private Double maxLongitude;
 }
