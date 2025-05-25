4cad85c282087ecb90353f2b69915aef68a8de30
dpfls0922 <dpfls0922@naver.com>
2025-05-25 20:05:54 +0900

    [DEL] 배치 작업 도입에 따른 주변시설 정보 저장 로직 제거 #27

--- src/main/java/com/ssafy/bango/domain/rentalhouse/controller/RentalHouseApiController.java
+++ src/main/java/com/ssafy/bango/domain/rentalhouse/controller/RentalHouseApiController.java
@@ -1,14 +1,11 @@
 package com.ssafy.bango.domain.rentalhouse.controller;
 
-import com.ssafy.bango.domain.rentalhouse.dto.request.FacilityRequest;
 import com.ssafy.bango.domain.rentalhouse.dto.response.GeoPointResponse;
-import com.ssafy.bango.domain.rentalhouse.service.KakaoFacilityService;
 import com.ssafy.bango.domain.rentalhouse.service.KakaoGeocodingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,18 +18,11 @@ import java.util.Optional;
 @RequestMapping("/api/v1/rental/api")
 public class RentalHouseApiController {
     private final KakaoGeocodingService geocodingService;
-    private final KakaoFacilityService facilityService;
 
     @GetMapping("/geocoding")
     public ResponseEntity<?> getGeoFromAddress(@RequestParam String address) {
         Optional<GeoPointResponse> geo = geocodingService.getGeoFromAddress(address);
         return geo.map(ResponseEntity::ok)
             .orElseGet(() -> ResponseEntity.noContent().build());
-    }
-
-    @GetMapping("/facilities/nearby")
-    public ResponseEntity<Void> saveClosestFacilities(@ModelAttribute FacilityRequest request) {
-        facilityService.saveClosestFacilities(request);
-        return ResponseEntity.noContent().build();
     }
 }
