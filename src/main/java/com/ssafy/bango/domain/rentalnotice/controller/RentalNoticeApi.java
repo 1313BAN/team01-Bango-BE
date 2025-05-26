cfd62ea5ed78d1747665840a91ecea2b8e3bd156
dpfls0922 <dpfls0922@naver.com>
2025-05-26 14:51:21 +0900

    [STYLE] 공고 관련 APi 스웨거 설명 수정 #27

--- src/main/java/com/ssafy/bango/domain/rentalnotice/controller/RentalNoticeApi.java
+++ src/main/java/com/ssafy/bango/domain/rentalnotice/controller/RentalNoticeApi.java
@@ -10,13 +10,13 @@ import java.security.Principal;
 
 @Tag(name = "임대 주택 공고 컨트롤러", description = "임대 주택 공고 관련 API입니다.")
 public interface RentalNoticeApi {
-    @Operation(summary = "임대 주택 공고 조회", description = "임대 주택 공고 조회하는 API 입니다.", responses = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "임대 주택 공고 조회 완료")
+    @Operation(summary = "dummy 임대 주택 공고 쌓기", description = "dummy 임대 주택 공고 데이터를 DB에 적재하는 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "dummy 임대 주택 공고 쌓기 완료")
     })
     void getRentalListTest();
 
-    @Operation(summary = "찜한 공고 주택 조회", description = "찜한 공고 주택을 조회하는 API 입니다.", responses = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "찜한 공고 주택 조회 완료")
+    @Operation(summary = "(찜한) 공고 주택 조회", description = "로그인 된 경우 (/), 로그인 되지 않은 경우(/like) 공고 주택을 조회하는 API 입니다.", responses = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "(찜한) 공고 주택 조회 완료")
     })
     ResponseEntity<ApiResponse<NoticeListResponseWithLiked>> getRentalListWithLike(int pageNo, int pageSize, Principal principal);
 }
