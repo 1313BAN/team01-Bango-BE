3a5278861b49864fd9508eaba0ce008367653dd8
dpfls0922 <dpfls0922@naver.com>
2025-05-26 14:51:00 +0900

    [ADD] 공고 관련 api를 AUTH_WHITELIST_WILDCARD에 추가 #27

--- src/main/java/com/ssafy/bango/global/auth/security/AuthWhiteList.java
+++ src/main/java/com/ssafy/bango/global/auth/security/AuthWhiteList.java
@@ -17,6 +17,7 @@ public class AuthWhiteList {
     public static final List<String> AUTH_WHITELIST_WILDCARD = Arrays.asList(
         "/api/v1/rental/**",
         "/api/v1/rental/api/**",
+        "/api/v1/notice/**",
         "/api/v1/ai/**",
         "/swagger-ui/**",
         "/v3/api-docs/**",
