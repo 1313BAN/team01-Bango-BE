d16af4389a405fea982c8356e48f78989aee5804
J2yun <alex68566856@gmail.com>
2025-05-27 09:11:26 +0900

    [FEAT] prometheus endpoint 화이트리스트에 추가 #32

--- src/main/java/com/ssafy/bango/global/auth/security/AuthWhiteList.java
+++ src/main/java/com/ssafy/bango/global/auth/security/AuthWhiteList.java
@@ -6,26 +6,29 @@ import java.util.stream.Stream;
 
 public class AuthWhiteList {
     public static final List<String> AUTH_WHITELIST_DEFAULT = Arrays.asList(
-        "/api/v1/member/login", "/api/v1/member/token",
-        "/api/v1/rental",
-        "/api/v1/notice",
-        "/batch-rentalhouse-job", "/batch-facility-job",
-        "/error", "/health",
-        "/swagger-ui.html"
+            "/api/v1/member/login", "/api/v1/member/token",
+            "/api/v1/rental",
+            "/api/v1/notice",
+            "/batch-rentalhouse-job", "/batch-facility-job",
+            "/error", "/health",
+            "/swagger-ui.html",
+            "/actuator"
     );
 
     public static final List<String> AUTH_WHITELIST_WILDCARD = Arrays.asList(
-        "/api/v1/rental/**",
-        "/api/v1/rental/api/**",
-        "/api/v1/notice/**",
-        "/api/v1/ai/**",
-        "/swagger-ui/**",
-        "/v3/api-docs/**",
-        "/swagger-resources/**"
+            "/api/v1/rental/**",
+            "/api/v1/rental/api/**",
+            "/api/v1/notice/**",
+            "/api/v1/dongcode/**",
+            "/api/v1/ai/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/actuator/**"
     );
 
     public static final String[] AUTH_WHITELIST = Stream.concat(
-        AUTH_WHITELIST_DEFAULT.stream(),
-        AUTH_WHITELIST_WILDCARD.stream()
+            AUTH_WHITELIST_DEFAULT.stream(),
+            AUTH_WHITELIST_WILDCARD.stream()
     ).toArray(String[]::new);
 }
